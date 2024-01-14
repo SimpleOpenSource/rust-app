5c744085d113cdf7de672602cb37b7ddab304117
Francis Lahoud <francis.lahoud@proton.me>
2024-01-14 18:33:48 -0500

    use api-lib

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,25 +1,9 @@
-use actix_web::{get, web::ServiceConfig};
+use api_lib::health::service;
+
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
-
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-
-    match result {
-        Ok(version) => version,
-        Err(e) => format!("Error: {:?}", e),
-    }
-}
-
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
 
 #[shuttle_runtime::main]
 async fn actix_web(
@@ -33,8 +17,7 @@ async fn actix_web(
     let config = move |cfg: &mut ServiceConfig| {
         cfg
             .app_data(pool)
-            .service(hello_world)
-            .service(version);
+            .configure(service);
     };
 
 
