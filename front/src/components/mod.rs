55df2df07ccfecbf301f77a7bcedd1560ed3dac2
Francis Lahoud <francis.lahoud@proton.me>
2024-01-15 17:32:19 -0500

    generate front end crud and modal

--- front/src/components/mod.rs
+++ front/src/components/mod.rs
@@ -1,5 +1,11 @@
+mod button;
 mod footer;
+mod film_card;
+mod film_modal;
 mod header;
 
+pub use button::Button;
 pub use footer::Footer;
+pub use film_card::FilmCard;
+pub use film_modal::FilmModal;
 pub use header::Header;
